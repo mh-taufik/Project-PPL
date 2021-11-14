52954eab4c83ba7b710be3d98d9318afe3be83fa
Muhammad Taufik <91010909+mh-taufik@users.noreply.github.com>
2021-11-14 19:58:25 +0700

    Fix BE

--- BE-Market/main.go
+++ BE-Market/main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"aph-go-service/transport"
-	_ "expvar"
+	"BEMarket/transport"
 	"net/http"
 	"os"
 
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 )
 
+
 func main() {
-
-	logger := log.NewLogfmtLogger(os.Stdout)
-
-	transport.RegisterHttpsServicesAndStartListener()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	logger.Log("listening-on", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		logger.Log("listen.error", err)
+	logger := log.NewLogfmtLogger(os.Stdout)     
+	transport.Router();   
+	port := os.Getenv("PORT")     
+	if port == "" {         
+		port = "8080"     
+		}     
+	logger.Log("listening-on", port)    
+	if err := http.ListenAndServe(":"+port, nil); err != nil {          
+		logger.Log("listen.error", err)     
 	}
 }
