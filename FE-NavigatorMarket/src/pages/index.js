b0dd2336a68b285bff667d400372c3a9bb60a5c8
chikitaS <chikita.salsabiil.tif20@polban.ac.id>
2021-11-15 07:59:43 +0700

    adding FE-NavigatorMarket project

---
+++ FE-NavigatorMarket/src/pages/index.js
@@ -0,0 +1,6 @@
+import feed from './feed'
+import goals from './goals'
+import home from './home'
+import profile from './profile'
+import search from './search'
+export {feed,goals,home,profile,search}
---
+++ FE-NavigatorMarket/src/pages/search/pages/index.js
@@ -0,0 +1,5 @@
+import friend from './friend'
+import forum from './forum'
+import event from './event'
+import market from './market'
+export {friend,forum,event,market};
