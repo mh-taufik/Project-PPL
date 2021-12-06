301b54ba96269e9ced8a3d8bf12f6cf18997c8c9
chikitaS <chikita.salsabiil.tif20@polban.ac.id>
2021-12-06 11:44:14 +0700

    making HomeScreen and ResultScreen

---
+++ FE-Market/src/code/HomeScreen.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {View, Text, StyleSheet, ScrollView} from 'react-native'
+import Header from './Header'
+import MenuBar from './MenuBar'
+import SearchBar from './SearchBar'
+import Produk from './Produk'
+import ShowingList from './Showinglist'
+
+
+const HomeScreen = () => {
+  return (
+    <View style={style.page}>
+      <Header/>
+      <SearchBar/>
+      <View style={{top: 10}}>
+        <ShowingList/>
+      </View>
+      <View style={style.page}>
+        <MenuBar/>
+      </View>
+    </View>
+  )
+}
+
+
+const style = StyleSheet.create({
+  page: {
+    backgroundColor: 'white',
+    height: 700,
+  }
+})
+
+export default HomeScreen;
