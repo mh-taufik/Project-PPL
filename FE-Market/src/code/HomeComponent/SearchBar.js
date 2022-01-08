b59e2665091567316269706d435c031618950bae
sarahazel <81137827+sarahazel@users.noreply.github.com>
2022-01-08 16:08:26 +0700

    update lagi

--- FE-Market/src/code/HomeComponent/SearchBar.js
+++ FE-Market/src/code/HomeComponent/SearchBar.js
@@ -1,31 +1,32 @@
 import * as React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
   Image,
   TextInput,
   TouchableOpacity,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
-const SearchBar = () => {
-  const [search, setSearch] = React.useState('');
+const { useState } = React;
+
+const SearchBar = ({selectedValue, chageValue}) => {
+  const [search, setSearch] = useState('');
   const navigation = useNavigation();
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState([]);
-  const [items, setItems] = React.useState([
-    {label: 'Dilihat Terbanyak', value: 1},
-    {label: 'Terjual Terbanyak', value: 2},
-    {label: 'Harga Tertinggi', value: 3},
-    {label: 'Harga Terendah', value: 4},
-    {label: 'Terbaru', value: 5},
+  const [open, setOpen] = useState(false);
+  // const [value, setValue] = useState([]);
+  const [items, setItems] = useState([
+    { label: 'Dilihat Terbanyak', value: 1 },
+    { label: 'Terjual Terbanyak', value: 2 },
+    { label: 'Harga Tertinggi', value: 3 },
+    { label: 'Harga Terendah', value: 4 },
+    { label: 'Terbaru', value: 5 },
   ]);
 
   return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-      <View style={{flexDirection: 'row'}}>
+    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+      <View style={{ flexDirection: 'row' }}>
         <TextInput
           placeholder="Search"
           style={style.bar}
@@ -36,7 +37,7 @@ const SearchBar = () => {
           <View>
             <TouchableOpacity
               onPress={() => {
-                navigation.navigate('Result', {key: userInput});
+                navigation.navigate('Result', { key: userInput });
               }}>
               <Image
                 style={style.button}
@@ -46,35 +47,38 @@ const SearchBar = () => {
           </View>
         </View>
       </View>
-      <View style={{flexDirection: 'row', top: 2, right: 10, width: 98}}>
-        <Image source={require('../../icon/filter.png')}
-            style={{width: 23, height: 22,left:-5,top:4}}/>
-        {/*<Text style={style.textBlue}>Filter</Text> */}
-
+      <View style={{ flexDirection: 'row', top: 2, right: 10, width: 98 }}>
+        <Image
+          source={require('../../icon/filter.png')}
+          style={{ width: 23, height: 22, left: -5, top: 4 }}
+        />
         <DropDownPicker
           open={open}
-          value={value}
+          value={selectedValue}
           items={items}
           setOpen={setOpen}
-          setValue={setValue}
+          setValue={chageValue}
           setItems={setItems}
           placeholder="Filter"
-          placeholderStyle={{color: '#0C8EFF'}}
+          placeholderStyle={{ color: '#0C8EFF' }}
           style={style.DropDown}
 
           listItemLabelStyle={{
             color: '#000',
             fontSize: 10,
           }}
+
           selectedItemContainerStyle={{
             backgroundColor: '#47aaff',
-            //'#7ac1ff'
           }}
 
           textStyle={{
             fontSize: 10,
           }}
-          
+
+          onChangeValue={(value) => {
+            return value;
+          }}
         />
       </View>
     </View>
@@ -86,7 +90,6 @@ const style = StyleSheet.create({
     color: '#868787',
     fontSize: 12,
     right: 10,
-    //left:1,
     width: 230,
     height: 33,
     borderColor: 'gray',
@@ -94,12 +97,6 @@ const style = StyleSheet.create({
     borderWidth: 1,
     paddingRight: 38,
     paddingLeft: 10,
-  },
-  textBlue: {
-    color: '#0C8EFF',
-    left: 2,
-    fontSize: 15,
-    top: 5,
   },
 
   button: {
@@ -112,22 +109,14 @@ const style = StyleSheet.create({
 
   DropDown: {
     color: '#868787',
-    //width : 130,
     height: 30,
     fontSize: 2,
     left: -2,
     top: -1,
-    //right:1,
     borderColor: 'gray',
     placeholder: 'Filter',
   },
-
-  // textSearch: {
-  //     color: '#868787',
-  //     fontSize: 12,
-  //     top: 4,
-  //     left: 10,
-  // }
+  
 });
 
 export default SearchBar;
